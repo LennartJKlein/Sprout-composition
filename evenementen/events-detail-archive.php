5eaaed4aa912827bb7acf603f6006db3506de50e
Jonathan Marchal (gmail) <marchalarts@gmail.com>
2018-04-11 21:39:09 +0200

    margins verbeterd voor mobile en archief-tag toegevoegd aan event detail archief

--- evenementen/events-detail-archive.php
+++ evenementen/events-detail-archive.php
@@ -12,6 +12,9 @@
                 <h1 class="event-title">The Next Web Conference 2018</h1>
                 <div class="event-intro">Op 24 en 25 mei vindt in Amsterdam de grootste techconferentie van Europa plaats: TNW Conference 2018. Tienduizend bezoekers vanuit de hele wereld komen hier luisteren naar sprekers als Constantijn van Oranje Nassau (StartupDelta), Anjali Sud (Vimeo), Mike Curtis (Airbnb), John Collison (Stripe) Rich Pierson (Headspace), Alex Le (Reddit) en vele anderen!</div>
                 <div class="event-hero figure">
+                    <div class="card-tags">
+                        <div class="card-tag">Archief</div>
+                    </div>
                     <img src="../images/events/the_next_web.jpg" class="figure-image" />
                 </div>
                 <div class="event-info">
