3e025d81f99b55f1367423882648f76d553cce2d
Lennart Klein <lennart.j.klein@gmail.com>
2018-06-11 17:49:33 +0200

    Created article type 'persbericht'

---
+++ artikel/persbericht.php
@@ -0,0 +1,216 @@
+<?php include '../templates/header.php'; ?>
+
+	<?php if(!isset($_COOKIE['sprout-hide-progress'])):  ?>
+	<div class="progressPanel">
+		<button class="progressPanel-close button button--small button--clean"><i class="fa fa-close"></i></button>
+		<ul class="progressPanel-list">
+			<li class="progressPanel-item">
+				<a href="7-redenen-waarom-klanten-je-winkel-de-rug-toekeren.php" class="progressPanel-link">
+					<span class="progressPanel-title">7 redenen waarom klanten je winkel de rug toekeren</span>
+					<span class="progressPanel-author">Fred Rutgers</span>
+					<svg x="0px" y="0px" width="36px" height="36px" viewBox="0 0 36 36" class="progressPanel-svg"><circle class="progressPanel-circle" fill="none" stroke="#2a76e8" stroke-width="2" cx="18" cy="18" r="16" stroke-dasharray="100 100" stroke-dashoffset="100" transform="rotate(-90 18 18)"></circle></svg>
+				</a>
+			</li>
+			<li class="progressPanel-item">
+				<a href="waarom-retailers-het-contact-met-jongeren-kwijt-zijn-geraakt.php" class="progressPanel-link">
+					<span class="progressPanel-title">Waarom retailers het contact met jongeren kwijt zijn geraakt</span>
+					<span class="progressPanel-author">Fred Rutgers</span>
+					<svg x="0px" y="0px" width="36px" height="36px" viewBox="0 0 36 36" class="progressPanel-svg"><circle class="progressPanel-circle" fill="none" stroke="#2a76e8" stroke-width="2" cx="18" cy="18" r="16" stroke-dasharray="100 100" stroke-dashoffset="100" transform="rotate(-90 18 18)"></circle></svg>
+				</a>
+			</li>
+			<li class="progressPanel-item">
+				<a href="hoe-overleef-je-tegenwoordig-als-winkel.php" class="progressPanel-link">
+					<span class="progressPanel-title">Hoe overleef je tegenwoordig als winkel? Ondernemers vertellen</span>
+					<span class="progressPanel-author">Jelmer Luimstra</span>
+					<svg x="0px" y="0px" width="36px" height="36px" viewBox="0 0 36 36" class="progressPanel-svg"><circle class="progressPanel-circle" fill="none" stroke="#2a76e8" stroke-width="2" cx="18" cy="18" r="16" stroke-dasharray="100 100" stroke-dashoffset="100" transform="rotate(-90 18 18)"></circle></svg>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="progressLine"></div>
+	<?php endif; ?>
+
+	<section class="articles">
+
+		<article class="article">
+			<header class="article-header">
+				<div class="article-tags">
+					<button class="button button--small button--grey" name="scaleups">Retail</button>
+					<button class="button button--small button--grey" name="investeren">Marketing</button>
+				</div>
+				<h1 class="article-title">7 redenen waarom klanten je winkel de rug toekeren</h1>
+				<div class="article-intro">Sprout-expert Fred Rutgers onderzocht waarom klanten een winkel de rug toekeren. &quot;De uitkomsten zullen voor menig winkelier best even schrikken zijn.&quot;</div>
+
+				<?php include '../templates/shareactions--wide.php' ?>
+
+				<div class="article-hero figure">
+					<img src="../images/articles/article-hero3.jpg" class="figure-image" />
+					<span class="figure-credit">&copy; Maikel Thijssen</span>
+				</div>
+				<div class="article-info">
+					<div class="articleMeta">
+						<date class="articleMeta-date">3 mei 2017 - 15:26</date>
+						<span class="articleMeta-readtime">7 min.</span>
+					</div>
+					<div class="articleAuthor">
+						<div class="articleAuthor-summary">
+							<a href="auteur.php" class="articleAuthor-link">
+								<img src="../images/fred-rutgers.jpg" class="articleAuthor-image" />
+								<span class="articleAuthor-name">Fred Rutgers</span>
+								<span class="articleAuthor-company">SchrijvenVoorWeb</span>
+							</a>
+							<span class="articleAuthor-toggle fa fa-chevron-down"></span>
+						</div>
+						<div class="articleAuthor-expand">
+							<p>Fred Rutgers is retailexpert en ondernemerscoach. Op basis van 30 jaar ervaring help ik retailers en ondernemers in de b2c sector om op een leuke manier en met minder stress te bouwen aan meer omzet...</p>
+							<div class="articleAuthor-social">
+								<a class="articleAuthor-sociallink button button--white button--small" href="#" target="_blank"><i class="fa fa-globe"></i>website</a>
+								<a class="articleAuthor-sociallink button button--white button--small" href="http://nl.linkedin.com/in/fredrutgers" target="_blank"><i class="fa fa-linkedin"></i>@fredrutgers</a>
+								<a class="articleAuthor-sociallink button button--white button--small" href="http://www.twitter.com/fredrutgers" target="_blank"><i class="fa fa-twitter"></i>fredrutgers</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<main class="article-content">
+				<p>Veel winkeliers denken dat klanten niet meer naar hun winkel komen omdat webshops goedkoper zijn. Vreemd genoeg bleek dit uit onderzoek maar zelden het geval.</p>
+				<p>Klanten zoeken niet perse de laagste prijs, maar ze willen wél de beste deal! Daarbij speelt vooral de totale koopbeleving een belangrijke rol.</p>
+				<p>Voor dit onderzoek werden 2000 consumenten ondervraagd via een enquête, en werden 20 consumenten ondervraagd in een consumenten-panel. Hieronder lees je de 7 belangrijkste uitkomsten:</p>
+				<h2>1. Welkom voelen</h2>
+				<p>Consumenten willen zich welkom voelen in een winkel. Dat heeft te maken met sfeer, verlichting, uitstraling van de winkel, maar bovenal met hoe medewerkers ze verwelkomen in de winkel. Hoe voelt de begroeting door het personeel. Komt die begroeting oprecht gemeend over of is het een plichtmatige begroeting zonder gevoel en oprechte interesse. Gebrek aan de juiste begroeting bepaalt voor een groot deel of de klant je de omzet gunt!</p>
+				
+				<h2>2. Meer dan geld</h2>
+				<p>Zie je de klant als mens of als een portemonnee waar je omzet uit moet trekken? Klanten willen voelen dat je ze echt wilt helpen en niet het idee krijgen dat je alleen uit bent op hun geld. Een veel gehoorde opmerking was dat consumenten het gevoel hebben dat één seconde na het afrekenen de aandacht al niet meer bij de klant is. Dat is vaak reden om niet meer of minder bij die winkel te komen.</p>
+				<h2>3. Vakkennis</h2>
+				<p>Als ze in de winkel geen kennis hebben van wat ze verkopen kun je beter naar een webshop gaan. Daar staat alle productinfo overzichtelijk op een rijtje en krijg ik zelfs alternatieve keuzes aangeboden. Krijgt een klant echter te maken met een verkoper met vakkennis en inlevingsvermogen in de echte behoefte van een klant, dan vinden klanten dat advies van echte toegevoegde waarde en zijn dan meestal graag bereid om meer te betalen.</p>
+				<h2>4. Toegevoegde waarde</h2>
+				<p>Als je in een winkel jouw toegevoegde waarde niet tastbaar of merkbaar kunt maken, waarom zouden klanten daar dan voor willen betalen? Een veel gehoorde klacht in het onderzoek is dat winkeliers te weinig moeite doen om een stapje extra te doen voor een klant. Bij minimale service hoort ook echt de minimale prijs, anders zoekt de klant elders wel de beste deal!</p>
+				<h2>5. Cadeautje</h2>
+				<p>Een belangrijke reden voor de klant om bij een winkel te kopen in plaats van bij een webshop is dat het gaat om een cadeau. Daarbij noemen ze twee redenen. De eerste is dat ruilen in een winkel gemakkelijker is dan bij een webshop. De ontvanger kan dan met minder moeite even zijn cadeau ruilen. Bovendien pakken webshops vrijwel nooit een cadeau leuk in. Als een winkel dat wel doet is dit een belangrijke reden om te kiezen voor die winkel. Zelfs als de klant eventueel iets extra moet betalen voor een luxe cadeauverpakking.</p>
+				<h2>6. Kletsend personeel</h2>
+				<p>Klanten haten personeel dat onderling praat tijdens het helpen van een klant! Erger nog vinden ze het als personeel “druk is op de telefoon” terwijl ze achter de toonbank of in de winkel staan. Voor veel klanten is dat één van de belangrijkste redenen om die winkel voortaan niet meer te bezoeken! Ook ergeren klanten zich enorm aan personeel dat niet achter de kassa of toonbank weg te slaan is terwijl de klant even advies vraagt of iets niet kan vinden.</p>
+				<p>Meer dan de helft van de klanten gaf aan in dergelijke gevallen van de koop af te zien! Voor bijna dertig procent was dat ook reden om voortaan een dergelijke winkel minder te bezoeken! </p>
+				<h2>7. Geen aftersales</h2>
+				<p>Gebrek aan aftersales aandacht is voor veel klanten reden om een winkel de rug toe te keren. Ruil beleid en garantieafwikkeling zijn grote bronnen van ergernis. Online mag je alles terugsturen, maar ruilen in een winkel is vaak een drama, zeker als je iets wilt terugbrengen dat niet naar behoren werkt. Vaak maken winkels een defect het probleem van de klant. Dat moet u rechtstreeks met de fabrikant regelen horen klanten te vaak. De nummer één reden om nooit meer terug te komen!</p>
+			</main>
+			<footer class="article-footer">
+				<?php include '../templates/shareactions.php' ?>
+
+				<div class="articleAuthor articleAuthor--bottom">
+					<div class="articleAuthor-content">
+						<a href="auteur.php" class="articleAuthor-link">
+							<img src="../images/fred-rutgers.jpg" class="articleAuthor-image" />
+							<span class="articleAuthor-name">Fred Rutgers</span>
+							<span class="articleAuthor-company">SchrijvenVoorWeb</span>
+						</a>
+						<p>Fred Rutgers is retailexpert en ondernemerscoach. 'Op basis van 30 jaar ervaring help ik retailers en ondernemers in de b2c sector om op een leuke manier en met minder stress te bouwen aan meer omzet...</p>
+						<div class="articleAuthor-social">
+							<a class="articleAuthor-sociallink button button--primary button--small" href="#" target="_blank"><i class="fa fa-globe"></i>website</a>
+							<a class="articleAuthor-sociallink button button--primary button--small" href="http://nl.linkedin.com/in/fredrutgers" target="_blank"><i class="fa fa-linkedin"></i>@fredrutgers</a>
+							<a class="articleAuthor-sociallink button button--primary button--small" href="http://www.twitter.com/fredrutgers" target="_blank"><i class="fa fa-twitter"></i>fredrutgers</a>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</article>
+		
+	</section>
+
+	<div class="relatedArticles">
+		<h4 class="relatedArticles-title">Andere persberichten hierover:</h4>
+		<div class="grid grid--articles">
+			<div class="grid-cell">
+				<a href="hoe-overleef-je-tegenwoordig-als-winkel.php" class="card card--horizontal card--premium" tag="retail">
+					<div class="card-figurecontainer">
+						<div class="card-tags">
+							<div class="card-tag">retail</div>
+						</div>
+						<div class="card-figure">
+							<img src="../images/articles/paul-efmorfidis-coco-mat-bed.jpg" class="card-image" />
+						</div>
+					</div>
+					<div class="card-content">
+						<h3 class="card-title">Hoe Coco-Mat vijftien landen verovert met duurzame producten</h3>
+						<p class="card-excerpt">Megasuccessen met duurzame bedden en linnengoed, het lukte de Griekse ondernemer Paul Efmorfidis (60). Vanuit Amsterdam bouwde hij zijn bedrijf Coco-Mat uit tot een wereldmerk, met winkels in New York en China.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid-cell">
+				<a href="hoe-overleef-je-tegenwoordig-als-winkel.php" class="card card--horizontal" tag="retail">
+					<div class="card-figurecontainer">
+						<div class="card-tags">
+							<div class="card-tag">retail</div>
+						</div>
+						<div class="card-figure">
+							<img src="../images/articles/foodora-overweegt-zzp-constructie.jpg" class="card-image" />
+						</div>
+					</div>
+					<div class="card-content">
+						<h3 class="card-title">Foodora overweegt zzp-constructie; burn-out voor 15 procent werknemers</h3>
+						<p class="card-excerpt">Ondernemersnieuws: Foodora overweegt zijn werknemers om te vormen tot zzp’ers, Uber verliest een vol geldpakhuis en de burn-out slaat om zich heen.</p>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<aside id="aside">
+
+	<div class="aside-panelgroup">				
+		<div class="panel panel--clean">
+			<ul class="eventList">
+				<li class="eventTeaser eventTeaser--minimal"><a class="eventTeaser-link" href="/evenementen/events.php">
+					<div class="eventTeaser-description">
+						<span class="eventTeaser-heading">Binnenkort</span>
+						<h4 class="eventTeaser-title">Amsterdam talks tech - smart cities &amp; mobility</h4>
+					</div>
+					<span class="eventTeaser-date">8 okt</span>
+				</a></li>
+				<li class="eventTeaser eventTeaser--minimal"><a class="eventTeaser-link" href="/evenementen/events.php">
+					<div class="eventTeaser-description">
+						<span class="eventTeaser-heading">Volgende maand</span>
+						<h4 class="eventTeaser-title">Scaleup summit 2017</h4>
+					</div>
+					<span class="eventTeaser-date">24 nov</span>
+				</a></li>
+			</ul>
+			<a href="#" class="button button--secondary button--clean button--center">Meer events</a>
+		</div>
+
+		<div class="banner banner--rectangle">
+			<img src="http://via.placeholder.com/285x140" class="banner-image" />
+		</div>
+	</div>
+
+	<div class="aside-panelgroup">
+		<div class="panel panel--headed panel--headed-overflow">
+			<h3 class="panel-title">Mede mogelijk door</h3>
+			<img src="../images/logo-postnl-channel-sprout.png" class="panel-figure" />
+			<a href="#" class="button button--center button--primary" onClick="openForm('register')">Whitepaper downloaden</a>
+		</div>
+
+		<div class="panel">
+			<h3 class="panel-title"><i class="panel-title-icon fa fa-folder"></i>Alles over...</h3>
+			<ul class="linkList">
+				<li class="linkList-item"><a class="linkList-link" href="/topic/direct-marketing.php">Retail</a></li>
+				<li class="linkList-item"><a class="linkList-link" href="/topic/direct-marketing.php">Crowdfunding</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+	<div class="aside-panelgroup">
+		<div class="banner banner--rectangle">
+			<img src="http://via.placeholder.com/285x140" class="banner-image" />
+		</div>
+
+		<div class="banner banner--rectangle">
+			<img src="http://via.placeholder.com/285x140" class="banner-image" />
+		</div>
+	</div>
+
+</aside>
+
+<?php include '../templates/footer.php'; ?>
