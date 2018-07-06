39c8fbf3119093771c360732e38e226552bb5cd6
Lennart Klein <lennart.j.klein@gmail.com>
2018-07-06 12:56:39 +0200

    Widgets to templates

--- artikel/waarom-retailers-het-contact-met-jongeren-kwijt-zijn-geraakt.php
+++ artikel/waarom-retailers-het-contact-met-jongeren-kwijt-zijn-geraakt.php
@@ -336,49 +336,21 @@
 
 <aside id="aside">
 
-	<div class="aside-panelgroup">				
-		<div class="panel panel--clean">
-			<ul class="eventList">
-				<li class="eventTeaser eventTeaser--minimal"><a class="eventTeaser-link" href="/events.php">
-					<div class="eventTeaser-description">
-						<span class="eventTeaser-heading">Binnenkort</span>
-						<h4 class="eventTeaser-title">Amsterdam talks tech - smart cities &amp; mobility</h4>
-					</div>
-					<span class="eventTeaser-date">8 okt</span>
-				</a></li>
-				<li class="eventTeaser eventTeaser--minimal"><a class="eventTeaser-link" href="/events.php">
-					<div class="eventTeaser-description">
-						<span class="eventTeaser-heading">Volgende maand</span>
-						<h4 class="eventTeaser-title">Scaleup summit 2017</h4>
-					</div>
-					<span class="eventTeaser-date">24 nov</span>
-				</a></li>
-			</ul>
-			<a href="#" class="button button--secondary button--clean button--center">Meer events</a>
-		</div>
-
-		<?php include '../templates/banners/banner--rectangle.php'; ?>
-	</div>
-
 	<div class="aside-panelgroup">
-		<div class="panel panel--headed panel--headed-overflow">
-			<h3 class="panel-title">Mede mogelijk door</h3>
-			<img src="../images/logo-postnl-channel-sprout.png" class="panel-figure" />
-			<a href="#" class="button button--center button--primary" onClick="openForm('register')">Whitepaper downloaden</a>
-		</div>
+		<?php include '../templates/banners/banner--rectangle.php'; ?>
 
-		<div class="panel">
-			<h3 class="panel-title"><i class="panel-title-icon fa fa-folder"></i>Alles over...</h3>
-			<ul class="linkList">
-				<li class="linkList-item"><a class="linkList-link" href="/topic/direct-marketing.php">Retail</a></li>
-				<li class="linkList-item"><a class="linkList-link" href="/topic/direct-marketing.php">Crowdfunding</a></li>
-			</ul>
-		</div>
+		<?php include '../templates/widgets/events.php'; ?>
 	</div>
-
 
 	<div class="aside-panelgroup">
 		<?php include '../templates/banners/banner--filmstrip.php'; ?>
+
+		<?php include '../templates/widgets/topics.php'; ?>
+	</div>
+
+
+	<div class="aside-panelgroup">
+		<?php include '../templates/widgets/partnerhighlight.php'; ?>
 
 		<?php include '../templates/banners/banner--rectangle.php'; ?>
 	</div>
---
+++ templates/widgets/partnerhighlight.php
@@ -0,0 +1,5 @@
+<div class="panel panel--headed panel--headed-overflow">
+	<h3 class="panel-title">Mede mogelijk door</h3>
+	<img src="../images/logo-postnl-channel-sprout.png" class="panel-figure" />
+	<a href="#" class="button button--center button--primary" onClick="openForm('register')">Whitepaper downloaden</a>
+</div>
