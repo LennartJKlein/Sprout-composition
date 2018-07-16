5cfcd8c76213a9f35c4a1bd707cb81cbee390d7c
Lennart Klein <lennart.j.klein@gmail.com>
2018-07-16 14:17:40 +0200

    Bugfix - quote font size

--- templates/widgets/premiummessage.php
+++ templates/widgets/premiummessage.php
@@ -3,7 +3,7 @@
 	<h3 class="premiumMessage-title">Dank dat je Sprout steunt.</h3>
 	<p class="premiumMessage-description">Dankzij jouw hulp kunnen we gave content blijven maken.</p>
 
-	<div class="panel quote">
+	<div class="quote">
 		<h4 class="quote-title">Quote van een topondernemer</h4>
 		<p class="quote-text">When something is important enough, you do it even if the odds are not in your favor</p>
 		<p class="quote-writer">Elon Musk</p>
