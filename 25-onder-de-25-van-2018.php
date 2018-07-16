0758ef727e6efd0339ab9dce3d5cd021c01d5a7d
Lennart Klein <lennart.j.klein@gmail.com>
2018-07-16 13:34:08 +0200

    Voorbeeld voor paginatype 'lijst' gebouwd

---
+++ 25-onder-de-25-van-2018.php
@@ -0,0 +1,246 @@
+<?php include 'templates/header.php'; ?>
+
+    <!-- Dit is een voorbeeld van een lijstpagina met cards naar andere pagina's -->
+
+    <div class="breadcrumb">
+        <a href="/index.php">Sprout.nl</a>
+        <a href="/25-onder-de-25-van-2018.php">25 onder de 25 van 2018</a>
+    </div>
+    <h1 class="main-title">25 onder de 25 van 2018</h1>
+    <div class="main-intro">
+        <p>Dit zijn de 25 meest veelbelovende ondernemers onder de 25 jaar van 2018. Ze zijn jong, barsten van het talent en de energie. Laat je niet afleiden door hun wilde haren en jonge overmoed, want voor je het weet zijn ze je met hun startups voorbij gestreefd. De 25 onder de 25 wordt elk jaar samengesteld door de redactie van Sprout.</p>
+        <p>Deel de lijst via<strong> <u><a href="http://twitter.com/share?url=https://www.sprout.nl/lijsten/25-onder-de-25-van-2018&amp;text=Dit zijn de beste jonge ondernemers van NL!&amp;via=sprout_nl" target="_blank">Twitter</a></u> | <u><a href="http://www.facebook.com/sharer.php?u=https://www.sprout.nl/lijsten/25-onder-de-25-van-2018" target="_blank">Facebook</a></u></strong> </p>
+        <p>Zie ook: <u><a href="/25-onder-de-25-van-2017" target="_blank">De 25 onder de 25 van 2017</a></u></p>
+    </div>
+    
+    <div class="grid grid--mobile-12 grid--tablet-6 grid--tabletlandscape-4 grid--desktop-3">
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card card--big card--vertical-mobile card--textfirst" tag="startups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/avant-arte.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Avant Arte</h3>
+                    <p class="card-excerpt">Christian Luiten (24), Curtis Penning (22)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card card--big card--vertical-mobile card--textfirst" tag="startups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/ballin-amsterdam.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Ballin Amsterdam</h3>
+                    <p class="card-excerpt">Auke de Geus (20)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card card--big card--textfirst" tag="startups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/birds-ai.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Birds.ai</h3>
+                    <p class="card-excerpt">Anouk Visser (25), Camiel Verschoor (27), Kitso Epema (28)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card card--big card--textfirst" tag="stories">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/boxrs4all.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Boxrs4ALL</h3>
+                    <p class="card-excerpt">Thom Uildriks (19), Spijk Geene (19)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/compendium-geneeskunde.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Compendium Geneeskunde</h3>
+                    <p class="card-excerpt">Veerle Smit (24)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/create.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Create</h3>
+                    <p class="card-excerpt">Martijn Apeldoorn (24)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="investeren">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/danielle-cathari.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Daniëlle Cathari</h3>
+                    <p class="card-excerpt">Daniëlle Cathari (23)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/festival-wallet.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Festival Wallet</h3>
+                    <p class="card-excerpt">Suzanne Eikmans (22)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag card-tag--premium"><i class="fa fa-star" aria-hidden="true"></i> premium</div>
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/gight.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Gight</h3>
+                    <p class="card-excerpt">Eef Lamers (24), Joris Olde Rikkert (21) en Robert van de Ven (29)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/grenzelooswerk.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Grenzelooswerk</h3>
+                    <p class="card-excerpt">Casper Lemmen (24), Rémon Wobbema (25) en Gijs Kingma (26)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="investeren">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/hemax.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">h&eacute;Max</h3>
+                    <p class="card-excerpt">Max van den Heuvel (22)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/hero-balancer.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Hero Balancer</h3>
+                    <p class="card-excerpt">Ted Braakman (19)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+        <div class="grid-cell">
+            <a href="/persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card" tag="scaleups">
+                <div class="card-figurecontainer">
+                    <div class="card-tags">
+                        <div class="card-tag card-tag--premium"><i class="fa fa-star" aria-hidden="true"></i> premium</div>
+                        <div class="card-tag">onderneming</div>
+                    </div>
+                    <div class="card-figure">
+                        <img src="images/uploads/holthausen-clean-technology.jpg" class="card-image" />
+                    </div>
+                </div>
+                <div class="card-content">
+                    <h3 class="card-title">Holthausen Clean Technology</h3>
+                    <p class="card-excerpt">Max Holthausen (19)</p>
+                    <span class="card-button">Meer lezen <i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                </div>
+            </a>
+        </div>
+    </div>
+        
+    <?php include 'templates/pagination.php'; ?>
+
+</section>
+        
+<?php include 'templates/footer.php'; ?>
