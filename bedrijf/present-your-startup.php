6bb133224ea88f03b900e63c1db9e8aece725797
Lennart Klein <lennart.klein@eo.nl>
2018-10-30 07:55:52 +0100

    Begin gemaakt met bedrijfsprofiel

---
+++ bedrijf/present-your-startup.php
@@ -0,0 +1,128 @@
+<?php include 'templates/header.php'; ?>
+
+	<div class="breadcrumb">
+		<a href="/index.php">Sprout.nl</a>
+		<a href="/bedrijven.php">Bedrijven</a>
+		<a href="#">PresentYourStartup</a>
+	</div>
+
+	<header class="main__header">
+
+		<h1 class="main__title">PresentYourStartup</h1>
+					
+		<div class="main__hero figure">
+			<img src="../images/uploads/main-hero.jpg" class="figure__image" />
+			<span class="figure__credit">&copy; Gerrit Postman</span>
+		</div>
+
+		<div class="main__intro">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+		</div>
+
+	</header>
+
+	<div class="main__body">
+
+		<h2>H2 Titel</h2>
+		<p><strong>Ondertitel of inleiding. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong></p>
+		<p>Sed accumsan felis quis neque sagittis pulvinar. Morbi mattis justo volutpat, imperdiet ante a, blandit risus. Proin porta risus at magna scelerisque placerat. Morbi mattis justo volutpat, imperdiet ante a, blandit risus. Proin porta risus at magna scelerisque placerat.</p>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Kolomnaam</th>
+					<th class="text--right">Andere kolom</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Consectetur adipiscing elit</td>
+					<td class="text--right">&euro;5,50</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Nulla eget purus pretium</td>
+					<td class="text--right">&euro;15,50</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Consectetur adipiscing elit</td>
+					<td class="text--right">&euro;5,50</td>
+				</tr>
+				<tr>
+					<td>6</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+				<tr>
+					<td>7</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<h2>H2 kop</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+
+		<h3>H3 kop</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+
+		<div class="panel">
+			<h3 class="panel__title">Lorem ipsum</h3>
+			<div class="panel__content">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+				<ul class="linkList">
+					<li class="linkList__item">
+						<a class="linkList__link" href="#" target="_new">Eerste link</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#" target="_new">Tweede link</a>
+					</li>
+				</ul>
+
+				<button class="button button--secondary button--center">Een goede doorverwijzing</button>
+
+			</div>
+		</div>
+
+		<h3>H3 kop</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper.</p>
+
+		<h4>H4 kop</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper.</p>
+
+		<div class="figure">
+			<img src="../images/uploads/articles/article-figure1.jpg" class="figure__image" />
+			<div class="figure__credit">
+				Boris Blijham en Danny ten Nijenhuis van ThePhoneLab.
+			</div>
+		</div>
+
+		<h4>H4 kop</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper.</p>
+
+		<h2>H2 kop</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+
+		<div class="buttonContainer text--center">
+			<button class="button button--secondary">Major optie</button>
+			<button class="button button--clean button--space button--secondary">Minor optie</button>
+		</div>
+
+	</div>
+
+</section>
+
+<?php include 'templates/footer.php'; ?>
---
+++ jobs.php
@@ -0,0 +1,213 @@
+<?php include 'templates/header.php'; ?>
+
+	<div class="breadcrumb">
+		<a href="/index.php">Sprout.nl</a>
+		<a href="/jobs.php">Sprout Jobs</a>
+	</div>
+
+	<header class="main__header">
+
+		<h1 class="main__title">Jobs that don't blow</h1>
+
+		<div class="main__intro">
+			<p>Vacatures van de meest relevante startups en scaleups.</p>
+		</div>
+
+	</header>
+
+	<div class="main__body">
+
+		<div class="buttonContainer text--center">
+			<button class="button button--secondary">Ik zoek talent</button>
+			<button class="button button--space button--primary">Ik wil graag werken bij een startup</button>
+		</div>
+
+		<div class="panel panel--headed panel--headed-big panel--headed-tertiary">
+			<h3 class="panel__title">Teamleader</h3>
+			<div class="panel__content">
+				<h4>Finance</h4>
+				<ul class="linkList">
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Accounting Specialist/Manager
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							International Payroll Officer
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Student Accountancy
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+				</ul>
+				<h4>Internships</h4>
+				<ul class="linkList">
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Digital Marketer Intern
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Graphic Designer - Intern
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Junior Developer - Intern
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+				</ul>
+				<h4>Customer Success</h4>
+				<ul class="linkList">
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Multilingual Customer Expert
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Customer Expert (Dutch - English - Italian)
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Customer Expert (Dutch - English - French)
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#">
+							Customer Success Hero
+							<span class="linkList__source"><i class="fa fa-location">Belgium, Gent</i></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th></th>
+					<th>Kolomnaam</th>
+					<th class="text--right">Andere kolom</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Consectetur adipiscing elit</td>
+					<td class="text--right">&euro;5,50</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Nulla eget purus pretium</td>
+					<td class="text--right">&euro;15,50</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Consectetur adipiscing elit</td>
+					<td class="text--right">&euro;5,50</td>
+				</tr>
+				<tr>
+					<td>6</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+				<tr>
+					<td>7</td>
+					<td>Lorem ipsum dolor sit amet</td>
+					<td class="text--right">&euro;5,00</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<h2>H2 kop</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+
+		<h3>H3 kop</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+
+		<div class="panel">
+			<h3 class="panel__title">Lorem ipsum</h3>
+			<div class="panel__content">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+				<ul class="linkList">
+					<li class="linkList__item">
+						<a class="linkList__link" href="#" target="_new">Eerste link</a>
+					</li>
+					<li class="linkList__item">
+						<a class="linkList__link" href="#" target="_new">Tweede link</a>
+					</li>
+				</ul>
+
+				<button class="button button--secondary button--center">Een goede doorverwijzing</button>
+
+			</div>
+		</div>
+
+		<h3>H3 kop</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper.</p>
+
+		<h4>H4 kop</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper.</p>
+
+		<div class="figure">
+			<img src="../images/uploads/articles/article-figure1.jpg" class="figure__image" />
+			<div class="figure__credit">
+				Boris Blijham en Danny ten Nijenhuis van ThePhoneLab.
+			</div>
+		</div>
+
+		<h4>H4 kop</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper.</p>
+
+		<h2>H2 kop</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget purus pretium, pretium nibh nec, imperdiet lacus. Etiam sollicitudin placerat iaculis. In sit amet vestibulum felis. Aenean lobortis bibendum justo, quis gravida turpis aliquet interdum. Sed eget pharetra libero. Sed tempor sed velit non dictum. Nullam in magna sit amet augue laoreet semper. Ut posuere ultrices mi vulputate egestas.</p>
+
+		<p>Sprout Jobs wordt mede-mogelijk gemaakt door <a href="https://recruitee.com/">Recruitee</a>, <a href="https://www.levelupventures.nl/">LevelUp Ventures</a> en <a href="https://www.taskhero.nl/">Taskhero</a>.</p>
+
+	</div>
+
+</section>
+
+<aside id="aside">
+	
+	<?php include 'templates/widgets/premiumteaser.php'; ?>
+
+	<?php include 'templates/widgets/premiummessage.php'; ?>
+	
+	<?php include 'templates/widgets/articles--partners.php'; ?>
+	
+	<?php include 'templates/widgets/events.php'; ?>
+
+	<?php include 'templates/widgets/topics.php'; ?>
+
+	<?php include 'templates/widgets/investments.php'; ?>
+
+	<?php include 'templates/widgets/externallinks.php'; ?>
+
+</aside>
+
+<?php include 'templates/footer.php'; ?>
