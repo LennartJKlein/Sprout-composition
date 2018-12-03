311c127664f386f1c7e16f2bc6a49cc4bd619ad7
Lennart Klein <lennart.klein@eo.nl>
2018-12-03 07:52:42 +0100

    Afbeeldingen voor vacatures toegevoegd

---
+++ jobs/teamleader-accounting-specialist-manager.php
@@ -0,0 +1,326 @@
+<?php include 'templates/header.php'; ?>
+
+	<div class="breadcrumb">
+		<a href="/index.php">Sprout.nl</a>
+		<a href="/jobs.php">Sprout Jobs</a>
+		<a href="/jobs/teamleader-accounting-specialist-manager.php">Vacature: Accounting Specialist/Manager</a>
+	</div>
+
+	<header class="main__header">
+
+		<h1 class="main__title">Vacature: Accounting Specialist/Manager</h1>
+
+		<div class="main__intro">
+			<p>Teamleader</p>
+		</div>
+
+		<div class="buttonContainer text--right">
+			<button class="button button--secondary"><i class="fa fa-plus"></i> Ik zoek talent</button>
+			<button class="button button--space button--primary"><i class="fa fa-paper-plane"></i> Ik wil graag werken bij een startup</button>
+		</div>
+
+	</header>
+
+	<div class="main__body">
+
+		<h2>Openstaande vacatures</h2>
+
+		<div class="panel panel--headed panel--headed-big panel--headed-tertiary">
+			<h3 class="panel__title">
+				<a href="https://www.google.com/" target="_new">Teamleader
+					<sup><i class="fa fa-info-circle"></i></sup>
+				</a>
+				<img src="/images/uploads/teamleader.jpg" class="panel__title-image" />
+			</h3>
+			<div class="panel__content">
+				<div class="grid grid--12 grid--tablet-6 grid--desktop-4">
+					<div class="grid__cell">
+						<h4>Finance</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Accounting Specialist/Manager
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									International Payroll Officer
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Student Accountancy
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="grid__cell">
+						<h4>Internships</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Digital Marketer Intern
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Graphic Designer - Intern
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Junior Developer - Intern
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="grid__cell">
+						<h4>Customer Success</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Multilingual Customer Expert
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Customer Expert (Dutch - English - Italian)
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Customer Expert (Dutch - English - French)
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Customer Success Hero
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="grid__cell">
+						<h4>Marketing</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Head of Growth Marketing
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Senior Marketer Belgium/The Netherlands
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Inbound Marketer
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="grid__cell">
+						<h4>Sales</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Sales Talent (NL)
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Amsterdam</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Sales Talent (BE)
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Country Manager Germany
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Germany, Berlin</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Inside Sales Executive (BE)
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Belgium, Gent</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="panel panel--headed panel--headed-big panel--headed-tertiary">
+			<h3 class="panel__title">
+				<a href="https://www.google.com/" target="_new">L1NDA
+					<sup><i class="fa fa-info-circle"></i></sup>
+				</a>
+				<img src="/images/uploads/l1nda.jpg" class="panel__title-image" />
+			</h3>
+			<div class="panel__content">
+				<div class="grid grid--12 grid--tablet-6 grid--desktop-4">
+					<div class="grid__cell">
+						<h4>Development</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Back-end (Python) developer
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Amsterdam</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="panel panel--headed panel--headed-big panel--headed-tertiary">
+			<h3 class="panel__title">
+				<a href="https://www.google.com/" target="_new">Semiotics Labs
+					<sup><i class="fa fa-info-circle"></i></sup>
+				</a>
+				<img src="/images/uploads/semioticslabs.jpg" class="panel__title-image" />
+			</h3>
+			<div class="panel__content">
+				<div class="grid grid--12 grid--tablet-6 grid--desktop-4">
+					<div class="grid__cell">
+						<h4>Operations</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Werkvoorbereider
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Leiden</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Project Manager
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Leiden</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="grid__cell">
+						<h4>Engineering</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Data Engineer
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Leiden</span>
+								</a>
+							</li>
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Software Engineer (backend)
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Leiden</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="panel panel--headed panel--headed-big panel--headed-tertiary">
+			<h3 class="panel__title">
+				<a href="https://www.google.com/" target="_new">InvoiceFinance
+					<sup><i class="fa fa-info-circle"></i></sup>
+				</a>
+				<img src="/images/uploads/invoicefinance.jpg" class="panel__title-image" />
+			</h3>
+			<div class="panel__content">
+				<div class="grid grid--12 grid--tablet-6 grid--desktop-4">
+					<div class="grid__cell">
+						<h4>Sales</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Growth Hacker
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Den Bosch</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="grid__cell">
+						<h4>Technology</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Full-stack Software Developer
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, 's Hertogenbosch</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="panel panel--headed panel--headed-big panel--headed-tertiary">
+			<h3 class="panel__title">
+				<a href="https://www.google.com/" target="_new">GeriMedica
+					<sup><i class="fa fa-info-circle"></i></sup>
+				</a>
+				<img src="/images/uploads/gerimedica.png" class="panel__title-image" />
+			</h3>
+			<div class="panel__content">
+				<div class="grid grid--12 grid--tablet-6 grid--desktop-4">
+					<div class="grid__cell">
+						<h4>Product development</h4>
+						<ul class="linkList">
+							<li class="linkList__item">
+								<a class="linkList__link" href="/jobs/teamleader-accounting-specialist-manager.php">
+									Java Software Engineer
+									<span class="linkList__source"><i class="fa fa-map-marker"></i>Netherlands, Amsterdam</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<p><em>Sprout Jobs wordt mede mogelijk gemaakt door <a href="https://recruitee.com/">Recruitee</a>, <a href="https://www.levelupventures.nl/">LevelUp Ventures</a> en <a href="https://www.taskhero.nl/">Taskhero</a>.</em></p>
+
+	</div>
+
+</section>
+
+<aside id="aside">
+	
+	<?php include 'templates/widgets/premiumteaser.php'; ?>
+
+	<?php include 'templates/widgets/premiummessage.php'; ?>
+	
+	<?php include 'templates/widgets/articles--partners.php'; ?>
+	
+	<?php include 'templates/widgets/events.php'; ?>
+
+	<?php include 'templates/widgets/topics.php'; ?>
+
+	<?php include 'templates/widgets/investments.php'; ?>
+
+	<?php include 'templates/widgets/externallinks.php'; ?>
+
+</aside>
+
+<?php include 'templates/footer.php'; ?>
