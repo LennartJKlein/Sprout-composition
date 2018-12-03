eeb9132e4f834f3d3bc772c5f83d3c97b384bbf0
Lennart Klein <lennart.klein@eo.nl>
2018-12-03 07:53:13 +0100

    Zoekresultatenpagina toegevoegd

---
+++ search.php
@@ -0,0 +1,247 @@
+<?php include 'templates/header.php'; ?>
+
+	<div class="breadcrumb">
+		<a href="/index.php">Sprout.nl</a>
+		<a href="/search.php">Zoekresultaten</a>
+	</div>
+
+	<header class="main__header">
+
+		<h1 class="main__title hidden-mobile hidden-tablet hidden-desktop">Zoekresultaten voor 'grow'</h1>
+
+		<div class="searchContainer">
+			<form class="search search--inpage">
+				<input type="text" placeholder="Zoeken..." value="grow" class="search__field" />
+				<button class="search__clear">
+					<i aria-hidden="true" class="fa fa-times-circle"></i>
+				</button>
+				<button type="submit" class="search__submit">
+					<i aria-hidden="true" class="fa fa-search"></i>
+				</button>
+			</form>
+			<div class="searchContainer__switches">
+				<a href="#" class="button button--small button--primary u-marginRs">alles (151)</a>
+				<a href="#" class="button button--small button--clean button--grey u-marginRs">artikelen (122)</a>
+				<a href="#" class="button button--small button--clean button--grey u-marginRs">ondernemingen (8)</a>
+				<a href="#" class="button button--small button--clean button--grey u-marginRs">lijsten (13)</a>
+				<a href="#" class="button button--small button--clean button--grey u-marginRs">onderwerpen (2)</a>
+			</div>
+			<hr class="searchContainer__divider u-marginTm u-marginBs" noshade/>
+			<p class="text--grey-dark u-marginBn"><small>151 resultaten voor 'grow'</small></p>
+		</div>
+
+	</header>
+
+	<div class="main__body">
+
+		<h3 class="text--primary">Ondernemingen</h3>
+		<div class="grid grid--6 grid--tablet-3">
+			<div class="grid__cell">
+				<a href="bedrijf/present-your-startup.php" class="card card--partner u-marginBs" tag="onderneming">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag">onderneming</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/partners/logo-postnl-channel-sprout.png" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">PostNL</h3>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="bedrijf/present-your-startup.php" class="card u-marginBs" tag="onderneming">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag">onderneming</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/hemax.jpg" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Hemax</h3>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="bedrijf/present-your-startup.php" class="card card--partner u-marginBs" tag="onderneming">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag">onderneming</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/festival-wallet.jpg" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">FestivalWallet</h3>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell u-alignSelf-center">
+				<a href="search.php" class="button button--clean">Meer laden (5)...</a>
+			</div>
+		</div>
+
+		<h3 class="text--primary">Artikelen</h3>
+		<div class="grid grid--12">
+			<div class="grid__cell">
+				<a href="artikel/hoe-overleef-je-tegenwoordig-als-winkel.php" class="card card--horizontal card--textfirst u-marginBm" tag="scaleups">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag">scaleups</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/articles/daan-reijnder-instantmagazine-profielfoto-sprout.jpg" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Hij verhuist met zijn gezin naar NY en deelt zijn ervaringen uit de Big Apple</h3>
+						<p class="card__excerpt">Ondernemer Daan Reijnders verblijft samen met zijn vrouw en 3 kinderen in New York om de nieuwe locatie van zijn bedrijf Instant Magazine op te starten.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="artikel/premium-artikel.php" class="card card--premium card--horizontal card--textfirst u-marginBm" tag="scaleups">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag card__tag--premium"><i class="fa fa-star" aria-hidden="true"></i> premium</div>
+							<div class="card__tag">scaleups</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/articles/schermafbeelding_2017-08-16_om_10.48.45.png" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Zo verklaart Uber de liefde aan managers</h3>
+						<p class="card__excerpt">Megasuccessen met duurzame bedden en linnengoed, het lukte de Griekse ondernemer Paul Efmorfidis (60). Vanuit Amsterdam bouwde hij zijn bedrijf Coco-Mat uit tot een wereldmerk, met winkels in New York en China.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="artikel/hoe-overleef-je-tegenwoordig-als-winkel.php" class="card card--horizontal card--textfirst u-marginBm" tag="scaleups">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag">scaleups</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/articles/paul-efmorfidis-coco-mat-bed.jpg" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Hoe Coco-Mat vijftien landen verovert met duurzame producten</h3>
+						<p class="card__excerpt">Megasuccessen met duurzame bedden en linnengoed, het lukte de Griekse ondernemer Paul Efmorfidis (60). Vanuit Amsterdam bouwde hij zijn bedrijf Coco-Mat uit tot een wereldmerk, met winkels in New York en China.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="persbericht/intershop-nieuwe-hoofdsponsor-van-ecommercerun.php" class="card card--partner card--horizontal card--textfirst u-marginBm" tag="investeren">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag card__tag--partner"><i class="fa fa-heart" aria-hidden="true"></i> partner</div>
+							<div class="card__tag">investeren</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/articles/bill_gates.png" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Hierom kan Bill Gates miljarden weggeven (infographic)</h3>
+						<p class="card__excerpt">Megasuccessen met duurzame bedden en linnengoed, het lukte de Griekse ondernemer Paul Efmorfidis (60). Vanuit Amsterdam bouwde hij zijn bedrijf Coco-Mat uit tot een wereldmerk, met winkels in New York en China.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="artikel/hoe-overleef-je-tegenwoordig-als-winkel.php" class="card card--horizontal card--textfirst u-marginBm" tag="scaleups">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag">scaleups</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/articles/daan-reijnder-instantmagazine-profielfoto-sprout.jpg" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Hij verhuist met zijn gezin naar NY en deelt zijn ervaringen uit de Big Apple</h3>
+						<p class="card__excerpt">Ondernemer Daan Reijnders verblijft samen met zijn vrouw en 3 kinderen in New York om de nieuwe locatie van zijn bedrijf Instant Magazine op te starten.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="artikel/premium-artikel.php" class="card card--premium card--horizontal card--textfirst u-marginBm" tag="scaleups">
+					<div class="card__figurecontainer">
+						<div class="card__tags">
+							<div class="card__tag card__tag--premium"><i class="fa fa-star" aria-hidden="true"></i> premium</div>
+							<div class="card__tag">scaleups</div>
+						</div>
+						<div class="card__figure">
+							<img src="images/uploads/articles/schermafbeelding_2017-08-16_om_10.48.45.png" class="card__image" />
+						</div>
+					</div>
+					<div class="card__content">
+						<h3 class="card__title">Zo verklaart Uber de liefde aan managers</h3>
+						<p class="card__excerpt">Megasuccessen met duurzame bedden en linnengoed, het lukte de Griekse ondernemer Paul Efmorfidis (60). Vanuit Amsterdam bouwde hij zijn bedrijf Coco-Mat uit tot een wereldmerk, met winkels in New York en China.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell grid__cell--wide">
+				<a href="search.php" class="button button--clean button--center">Meer laden (116)...</a>
+			</div>
+		</div>
+
+		<h3 class="text--primary">Lijsten</h3>
+		<div class="grid grid--12">
+			<div class="grid__cell">
+				<a href="lijst/25-onder-de-25-van-2018.php" class="card card--horizontal card--textfirst u-marginBm" tag="lijst">
+					<div class="card__content">
+						<h3 class="card__title">25 onder de 25 van 2018</h3>
+						<p class="card__excerpt">Dit zijn de 25 meest veelbelovende ondernemers onder de 25 jaar van 2018. Ze zijn jong, barsten van het talent en de energie. Laat je niet afleiden door hun wilde haren en jonge overmoed, want voor je het weet zijn ze je met hun startups voorbij gestreefd. De 25 onder de 25 wordt elk jaar samengesteld door de redactie van Sprout.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="lijst/25-onder-de-25-van-2018.php" class="card card--horizontal card--textfirst u-marginBm" tag="lijst">
+					<div class="card__content">
+						<h3 class="card__title">10 manieren om salarisverhoging te vragen</h3>
+						<p class="card__excerpt">Lorum ipsum dolor sit amet consectetur adscipiing elit. Mini tracht hun wilde haren en jonge overmoed, want voor je het weet zijn ze je met hun startups voorbij gestreefd. De 25 redactie van Sprout. Lorum ipsum dolor sit amet consectetur adscipiing elit.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="lijst/25-onder-de-25-van-2018.php" class="card card--horizontal card--textfirst u-marginBm" tag="lijst">
+					<div class="card__content">
+						<h3 class="card__title">Nederlandse Scaleups met succesvolle franchises in het buitenland</h3>
+						<p class="card__excerpt">Dit zijn de Scaleups die het hebben gemaakt in het buitenland. Lorum ipsum dolor sit amet consectetur adscipiing elit. 25 meest veelbelovende ondernemers onder de 25 jaar van 2018. Ik ben samengesteld door de redactium dolor sit amet consectetur adscipiing.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell">
+				<a href="lijst/25-onder-de-25-van-2018.php" class="card card--horizontal card--textfirst u-marginBm" tag="lijst">
+					<div class="card__content">
+						<h3 class="card__title">25 onder de 25 van 2018</h3>
+						<p class="card__excerpt">Dit zijn de 25 meest veelbelovende ondernemers onder de 25 jaar van 2018. Ze zijn jong, barsten van het talent en de energie. Laat je niet afleiden door hun wilde haren en jonge overmoed, want voor je het weet zijn ze je met hun startups voorbij gestreefd. De 25 onder de 25 wordt elk jaar samengesteld door de redactie van Sprout.</p>
+					</div>
+				</a>
+			</div>
+			<div class="grid__cell grid__cell--wide">
+				<a href="search.php" class="button button--clean button--center">Meer laden (9)...</a>
+			</div>
+		</div>
+
+		<h3 class="text--primary">Onderwerpen</h3>
+		<div class="grid grid--12">
+			<div class="grid__cell">
+				<a href="topic/direct-marketing.php" class="button">Direct Marketing</a>
+				<a href="topic/persberichten.php" class="button">Persberichten</a>
+			</div>
+		</div>
+
+
+	</div>
+
+</section>
+
+<?php include 'templates/footer.php'; ?>
